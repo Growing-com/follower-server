930f3dc6a8aaa6b317c4da8f1059e4d93352eeb6
imcool2551 <imcool2551@gmail.com>
2022-09-01 22:43:07 +0900

    Initial commit

---
+++ src/test/java/com/sarangchurch/follower/FollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarangchurch.follower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
